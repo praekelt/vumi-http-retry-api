2c2f4054f1a9ce0d81dd4779267ba20d825efa87
justinvdm <justinvdm.0.1.0@gmail.com>
2015-07-24 11:36:10 +0200

    Python project boilerplate

---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. vumi-http-retry-api documentation master file, created by
+   sphinx-quickstart on Fri Jul 24 11:31:34 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to vumi-http-retry-api's documentation!
+===============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ utils/bump-version.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+VER="$1"
+
+if [[ "x${VER}" = "x" ]]
+then
+    echo "Usage: $0 <version number>"
+    echo " e.g. $0 0.1.0"
+    exit 1
+fi
+
+function inplace_sed {
+    # Note: we don't use sed -i -e ... because it isn't supported by FreeBSD
+    # sed on OS X.
+    local command="$1"; shift
+    local suffix=".inplace.bak"
+    sed -i"${suffix}" -e "${command}" "$@"
+    for filename in "$@"; do
+        rm "${filename}${suffix}"
+    done
+}
+
+SHORT_VER=`echo "${VER}" | sed -e "s/\.[^.]*$//"`
+
+setup_sed="s/\(version[ ]*=[ ]*[\"']\)\(.*\)\([\"'].*\)/\1${VER}\3/"
+init_sed="s/^\(__version__[ ]*=[ ]*[\"']\)\(.*\)\([\"'].*\)/\1${VER}\3/"
+
+inplace_sed "${setup_sed}" setup.py 
+inplace_sed "${init_sed}" vumi_http_retry/__init__.py
+inplace_sed "s/^\(release[ ]*=[ ]*[\"']\)\(.*\)\([\"'].*\)/\1${VER}\3/" docs/conf.py
+inplace_sed "s/^\(version[ ]*=[ ]*[\"']\)\(.*\)\([\"'].*\)/\1${SHORT_VER}\3/" docs/conf.py
+
+git add setup.py vumi_http_retry/__init__.py docs/conf.py
