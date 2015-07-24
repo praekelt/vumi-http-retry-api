26d5ef58aa93d688dc63235ce6d296ad2cb0000f
justinvdm <justinvdm.0.1.0@gmail.com>
2015-07-24 14:15:10 +0200

    Better description for twistd service

--- twisted/plugins/vumi_http_retry.py
+++ twisted/plugins/vumi_http_retry.py
@@ -4,5 +4,5 @@ from twisted.application.service import ServiceMaker
 serviceMaker = ServiceMaker(
     'vumi_http_retry',
     'vumi_http_retry.service',
-    'vumi_http_retry',
+    'API to retry HTTP requests',
     'vumi_http_retry')
