62474424658de65546db8cf756cca7d49af250ff
justinvdm <justinvdm.0.1.0@gmail.com>
2015-07-29 01:57:56 +0200

    Support the basics moving requests from pending to ready

--- vumi_http_retry/workers/maintainer/tests/test_worker.py
+++ vumi_http_retry/workers/maintainer/tests/test_worker.py
@@ -1,5 +1,75 @@
+from twisted.internet.task import Clock
 from twisted.trial.unittest import TestCase
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+from vumi_http_retry.workers.maintainer.worker import RetryMaintainerWorker
+from vumi_http_retry.retries import pending_key, ready_key, add_pending
+from vumi_http_retry.tests.redis import zitems, lvalues, delete
 
 
 class TestRetryMaintainerWorker(TestCase):
-    pass
+    @inlineCallbacks
+    def teardown_worker(self, worker):
+        yield delete(worker.redis, 'test.*')
+        yield worker.teardown()
+
+    @inlineCallbacks
+    def mk_worker(self, config):
+        config['redis_pefix'] = 'test'
+        worker = RetryMaintainerWorker(config)
+
+        yield worker.setup(Clock())
+        self.addCleanup(self.teardown_worker, worker)
+
+        worker.stop_maintain_loop()
+        returnValue(worker)
+
+    @inlineCallbacks
+    def test_maintain(self):
+        k_p = pending_key('test')
+        k_r = ready_key('test')
+
+        worker = yield self.mk_worker({
+            'redis_prefix': 'test',
+            'frequency': 20,
+        })
+
+        for t in range(5, 25, 5):
+            yield add_pending(worker.redis, 'test', {
+                'owner_id': '1234',
+                'timestamp': t,
+                'attempts': 0,
+                'intervals': [10],
+                'request': {'foo': t}
+            })
+
+        pending = yield zitems(worker.redis, pending_key('test'))
+        pending_reqs = [v for t, v in pending]
+
+        worker.clock.advance(10)
+        self.assertEqual((yield lvalues(worker.redis, k_r)), [])
+        self.assertEqual((yield zitems(worker.redis, k_p)), pending)
+        yield worker.maintain()
+        self.assertEqual((yield lvalues(worker.redis, k_r)), [])
+        self.assertEqual((yield zitems(worker.redis, k_p)), pending)
+
+        worker.clock.advance(5)
+        self.assertEqual((yield lvalues(worker.redis, k_r)), [])
+        self.assertEqual((yield zitems(worker.redis, k_p)), pending)
+        yield worker.maintain()
+        self.assertEqual((yield lvalues(worker.redis, k_r)), pending_reqs[:1])
+        self.assertEqual((yield zitems(worker.redis, k_p)), pending[1:])
+
+        worker.clock.advance(10)
+        self.assertEqual((yield lvalues(worker.redis, k_r)), pending_reqs[:1])
+        self.assertEqual((yield zitems(worker.redis, k_p)), pending[1:])
+        yield worker.maintain()
+        self.assertEqual((yield lvalues(worker.redis, k_r)), pending_reqs[:3])
+        self.assertEqual((yield zitems(worker.redis, k_p)), pending[3:])
+
+        worker.clock.advance(5)
+        self.assertEqual((yield lvalues(worker.redis, k_r)), pending_reqs[:3])
+        self.assertEqual((yield zitems(worker.redis, k_p)), pending[3:])
+        yield worker.maintain()
+        self.assertEqual((yield lvalues(worker.redis, k_r)), pending_reqs)
+        self.assertEqual((yield zitems(worker.redis, k_p)), [])
--- vumi_http_retry/workers/maintainer/worker.py
+++ vumi_http_retry/workers/maintainer/worker.py
@@ -1,17 +1,60 @@
+from twisted.internet import reactor
+from twisted.internet.protocol import ClientCreator
+from twisted.internet.defer import inlineCallbacks
+
+from txredis.client import RedisClient
 from confmodel import Config
+from confmodel.fields import ConfigText, ConfigInt
 
 from vumi_http_retry.worker import BaseWorker
+from vumi_http_retry.retries import pop_pending, add_ready
 
 
 class RetryMaintainerConfig(Config):
-    pass
+    frequency = ConfigInt(
+        "How often the working set should be updated (in seconds)",
+        default=60)
+    redis_prefix = ConfigText(
+        "Prefix for redis keys",
+        default='vumi_http_retry')
+    redis_host = ConfigText(
+        "Redis client host",
+        default='localhost')
+    redis_port = ConfigInt(
+        "Redis client port",
+        default=6379)
 
 
 class RetryMaintainerWorker(BaseWorker):
     CONFIG_CLS = RetryMaintainerConfig
 
-    def setup(self):
-        pass
+    @inlineCallbacks
+    def setup(self, clock=None):
+        if clock is None:
+            clock = reactor
+
+        self.clock = clock
+
+        redisCreator = ClientCreator(reactor, RedisClient)
+
+        self.redis = yield redisCreator.connectTCP(
+            self.config.redis_host,
+            self.config.redis_port)
+
+        self.start_maintain_loop()
 
     def teardown(self):
+        self.redis.transport.loseConnection()
+        self.stop_maintain_loop()
+
+    @inlineCallbacks
+    def maintain(self):
+        prefix = self.config.redis_prefix
+        reqs = yield pop_pending(self.redis, prefix, 0, self.clock.seconds())
+        yield add_ready(self.redis, prefix, reqs)
+
+    def start_maintain_loop(self):
+        pass
+
+    def stop_maintain_loop(self):
         pass
