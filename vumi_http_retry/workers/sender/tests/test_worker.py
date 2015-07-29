bea87314573e8195008b68618caddeac896c321b
justinvdm <justinvdm.0.1.0@gmail.com>
2015-07-29 18:50:28 +0200

    Add the basic retry logic for retry sender worker's retry()

--- vumi_http_retry/workers/sender/tests/test_worker.py
+++ vumi_http_retry/workers/sender/tests/test_worker.py
@@ -1,5 +1,146 @@
 from twisted.trial.unittest import TestCase
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+from vumi_http_retry.workers.sender.worker import RetrySenderWorker
+from vumi_http_retry.retries import pending_key
+from vumi_http_retry.tests.redis import zitems, delete
+from vumi_http_retry.tests.utils import ToyServer
+
+
+class ToyRetrySenderWorker(RetrySenderWorker):
+    pass
 
 
 class TestRetrySenderWorker(TestCase):
-    pass
+    @inlineCallbacks
+    def teardown_worker(self, worker):
+        yield delete(worker.redis, 'test.*')
+        yield worker.teardown()
+
+    @inlineCallbacks
+    def mk_worker(self, config=None):
+        if config is None:
+            config = {}
+
+        config['redis_prefix'] = 'test'
+        config.setdefault('overrides', {}).update({'persistent': False})
+
+        worker = ToyRetrySenderWorker(config)
+        yield worker.setup()
+        self.addCleanup(self.teardown_worker, worker)
+
+        returnValue(worker)
+
+    @inlineCallbacks
+    def test_retry(self):
+        worker = yield self.mk_worker()
+        srv = yield ToyServer.from_test(self)
+        reqs = []
+
+        @srv.app.route('/foo')
+        def route(req):
+            reqs.append(req)
+
+        yield worker.retry({
+            'owner_id': '1234',
+            'timestamp': 5,
+            'attempts': 0,
+            'intervals': [10],
+            'request': {
+                'url': "%s/foo" % (srv.url,),
+                'method': 'POST'
+            }
+        })
+
+        [req] = reqs
+        self.assertEqual(req.method, 'POST')
+        self.assertEqual((yield zitems(worker.redis, pending_key('test'))), [])
+
+    @inlineCallbacks
+    def test_retry_reschedule(self):
+        worker = yield self.mk_worker()
+        srv = yield ToyServer.from_test(self)
+
+        @srv.app.route('/foo')
+        def route(req):
+            req.setResponseCode(500)
+
+        yield worker.retry({
+            'owner_id': '1234',
+            'timestamp': 5,
+            'attempts': 0,
+            'intervals': [10, 20],
+            'request': {
+                'url': "%s/foo" % (srv.url,),
+                'method': 'POST'
+            }
+        })
+
+        yield worker.retry({
+            'owner_id': '1234',
+            'timestamp': 10,
+            'attempts': 0,
+            'intervals': [10, 30],
+            'request': {
+                'url': "%s/foo" % (srv.url,),
+                'method': 'POST'
+            }
+        })
+
+        pending = yield zitems(worker.redis, pending_key('test'))
+
+        self.assertEqual(pending, [
+            (5 + 20, {
+                'owner_id': '1234',
+                'timestamp': 5,
+                'attempts': 1,
+                'intervals': [10, 20],
+                'request': {
+                    'url': "%s/foo" % (srv.url,),
+                    'method': 'POST'
+                }
+            }),
+            (10 + 30, {
+                'owner_id': '1234',
+                'timestamp': 10,
+                'attempts': 1,
+                'intervals': [10, 30],
+                'request': {
+                    'url': "%s/foo" % (srv.url,),
+                    'method': 'POST'
+                }
+            })
+        ])
+
+    @inlineCallbacks
+    def test_retry_end(self):
+        worker = yield self.mk_worker()
+        srv = yield ToyServer.from_test(self)
+
+        @srv.app.route('/foo')
+        def route(req):
+            req.setResponseCode(500)
+
+        yield worker.retry({
+            'owner_id': '1234',
+            'timestamp': 5,
+            'attempts': 1,
+            'intervals': [10, 20],
+            'request': {
+                'url': "%s/foo" % (srv.url,),
+                'method': 'POST'
+            }
+        })
+
+        yield worker.retry({
+            'owner_id': '1234',
+            'timestamp': 10,
+            'attempts': 2,
+            'intervals': [10, 30, 40],
+            'request': {
+                'url': "%s/foo" % (srv.url,),
+                'method': 'POST'
+            }
+        })
+
+        self.assertEqual((yield zitems(worker.redis, pending_key('test'))), [])
--- vumi_http_retry/workers/sender/worker.py
+++ vumi_http_retry/workers/sender/worker.py
@@ -1,17 +1,51 @@
+from twisted.internet import reactor
+from twisted.internet.protocol import ClientCreator
+from twisted.internet.defer import inlineCallbacks
+
+from txredis.client import RedisClient
 from confmodel import Config
+from confmodel.fields import ConfigText, ConfigInt, ConfigDict
 
 from vumi_http_retry.worker import BaseWorker
+from vumi_http_retry.retries import (
+    retry, should_retry, can_reattempt, add_pending)
 
 
 class RetrySenderConfig(Config):
-    pass
+    frequency = ConfigInt(
+        "How often the ready set should be polled (in seconds)",
+        default=60)
+    redis_prefix = ConfigText(
+        "Prefix for redis keys",
+        default='vumi_http_retry')
+    redis_host = ConfigText(
+        "Redis client host",
+        default='localhost')
+    redis_port = ConfigInt(
+        "Redis client port",
+        default=6379)
+    overrides = ConfigDict(
+        "Options to override for each request",
+        default={})
 
 
 class RetrySenderWorker(BaseWorker):
     CONFIG_CLS = RetrySenderConfig
 
+    @inlineCallbacks
     def setup(self):
-        pass
+        redisCreator = ClientCreator(reactor, RedisClient)
+
+        self.redis = yield redisCreator.connectTCP(
+            self.config.redis_host,
+            self.config.redis_port)
 
     def teardown(self):
-        pass
+        self.redis.transport.loseConnection()
+
+    @inlineCallbacks
+    def retry(self, req):
+        resp = yield retry(req, **self.config.overrides)
+
+        if should_retry(resp) and can_reattempt(req):
+            yield add_pending(self.redis, self.config.redis_prefix, req)
