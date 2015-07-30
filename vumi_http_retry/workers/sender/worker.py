f2e9566287d844e6a0f23cbcd79023726a1cad75
justinvdm <justinvdm.0.1.0@gmail.com>
2015-07-30 13:49:50 +0200

    More cleanup

--- vumi_http_retry/workers/sender/worker.py
+++ vumi_http_retry/workers/sender/worker.py
@@ -1,7 +1,7 @@
 from twisted.internet import reactor
 from twisted.internet.task import LoopingCall
 from twisted.internet.protocol import ClientCreator
-from twisted.internet.defer import inlineCallbacks, Deferred
+from twisted.internet.defer import inlineCallbacks, Deferred, succeed
 
 from txredis.client import RedisClient
 from confmodel import Config
@@ -63,6 +63,7 @@ class RetrySenderWorker(BaseWorker):
 
         self.loop = LoopingCall(self.poll)
         self.loop.clock = self.clock
+        self.loop_d = succeed(None)
 
         self.start()
 
@@ -81,11 +82,6 @@ class RetrySenderWorker(BaseWorker):
         if should_retry(resp) and can_reattempt(req):
             yield add_pending(self.redis, self.config.redis_prefix, req)
 
-    def safe_to_stop(self):
-        if self.stopping:
-            self.state = 'stopped'
-            self.stopping_d.callback(None)
-
     @inlineCallbacks
     def poll(self):
         while True:
@@ -97,11 +93,9 @@ class RetrySenderWorker(BaseWorker):
             if req:
                 yield self.retry(req)
 
-        self.safe_to_stop()
-
     def start(self):
         self.state = 'started'
-        self.loop.start(self.config.frequency, now=True)
+        self.loop_d = self.loop.start(self.config.frequency, now=True)
 
     @inlineCallbacks
     def stop(self):
@@ -113,4 +107,5 @@ class RetrySenderWorker(BaseWorker):
         if self.loop.running:
             self.loop.stop()
 
-        yield self.stopping_d
+        yield self.loop_d
+        self.state = 'stopped'
