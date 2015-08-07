4c67b878beab4397eee9ff5e8560b8a996fe8aba
justinvdm <justinvdm.0.1.0@gmail.com>
2015-08-07 14:29:29 +0200

    Change DeferredLimiter to instead return deferreds on .add() calls that callback when there is space for the given task to be run (and rename it to TaskLimiter to match the change in behaviour)

--- vumi_http_retry/limiter.py
+++ vumi_http_retry/limiter.py
@@ -1,65 +1,55 @@
-from twisted.internet.defer import Deferred
+from twisted.internet.defer import (
+    Deferred, inlineCallbacks, DeferredLock, maybeDeferred)
 
 
-class DeferredLimiterOverflow(Exception):
-    """Raised when one attempts to add to a full DeferredLimiter"""
-
-
-class DeferredLimiter(object):
+class TaskLimiter(object):
     """
-    Keeps track of a limited list of pending deferreds, allowing one to wait
-    for a vacancy in the list when one of the deferreds is done.
+    Keeps track of a limited list of pending tasks, allowing one to request for
+    a task to be added once there is a vacancy in the list.
     """
     def __init__(self, limit, errback=None):
+        self.lock = DeferredLock()
         self.limit = limit
-        self.deferreds = []
         self.observers = []
+        self.tasks = set()
 
         if errback is None:
             errback = lambda f: f
 
         self.errback = errback
 
-    @property
-    def full(self):
-        return self.count >= self.limit
+    @inlineCallbacks
+    def add(self, fn, *a, **kw):
+        observer = Deferred()
+        self.observers.append(observer)
+        observer.addCallback(self._run, fn, *a, **kw)
+        yield self._refresh()
+        yield observer
 
-    @property
-    def count(self):
-        return len(self.deferreds)
-
-    def add(self, d):
-        if self.full:
-            raise DeferredLimiterOverflow()
-
-        self.deferreds.append(d)
+    def _run(self, _, fn, *a, **kw):
+        d = maybeDeferred(fn, *a, **kw)
+        self.tasks.add(d)
         d.addCallback(self._callback, d)
         d.addErrback(self._errback, d)
-        return d
 
-    def when_not_full(self):
-        d = Deferred()
-        self.observers.append(d)
-        self._refresh()
-        return d
-
-    def cancel(self):
-        for d in self.deferreds[:]:
-            d.cancel()
-
+    @inlineCallbacks
     def _callback(self, _, d):
-        self.deferreds.remove(d)
-        self._refresh()
+        self.tasks.remove(d)
+        yield self._refresh()
 
-    def _refresh(self):
-        if self.full:
-            return
-
-        for d in self.observers:
-            d.callback(self.count)
-
-        self.observers = []
-
+    @inlineCallbacks
     def _errback(self, f, d):
-        self._callback(None, d)
+        yield self._callback(None, d)
         self.errback(f)
+
+    @inlineCallbacks
+    def _refresh(self):
+        yield self.lock.acquire()
+
+        if self.observers and not self._is_full():
+            self.observers.pop(0).callback(None)
+
+        self.lock.release()
+
+    def _is_full(self):
+        return len(self.tasks) >= self.limit
--- vumi_http_retry/tests/test_limiter.py
+++ vumi_http_retry/tests/test_limiter.py
@@ -1,132 +1,86 @@
 from twisted.trial.unittest import TestCase
 from twisted.internet.defer import Deferred, inlineCallbacks
 
-from vumi_http_retry.limiter import DeferredLimiterOverflow, DeferredLimiter
+from vumi_http_retry.tests.utils import ManualWritable
+from vumi_http_retry.limiter import TaskLimiter
 
 
-class TestDeferredLimiter(TestCase):
-    def test_full(self):
-        limiter = DeferredLimiter(2)
-        self.assertFalse(limiter.full)
-
-        limiter.add(Deferred())
-        self.assertFalse(limiter.full)
-
-        limiter.add(Deferred())
-        self.assertTrue(limiter.full)
-
-    def test_count(self):
-        limiter = DeferredLimiter(2)
-        self.assertEqual(limiter.count, 0)
-
-        limiter.add(Deferred())
-        self.assertEqual(limiter.count, 1)
-
-        limiter.add(Deferred())
-        self.assertEqual(limiter.count, 2)
-
-    def test_add_overflow(self):
-        limiter = DeferredLimiter(2)
-        limiter.add(Deferred())
-        limiter.add(Deferred())
-        self.assertRaises(DeferredLimiterOverflow, limiter.add, Deferred)
-        self.assertRaises(DeferredLimiterOverflow, limiter.add, Deferred)
-
+class TestTaskLimiter(TestCase):
     @inlineCallbacks
-    def test_when_not_full(self):
-        limiter = DeferredLimiter(2)
-        d1 = limiter.add(Deferred())
-        limiter.add(Deferred())
-        d = limiter.when_not_full()
+    def test_add(self):
+        limiter = TaskLimiter(2)
+        w = ManualWritable()
 
-        self.assertTrue(limiter.full)
-        d1.callback(None)
-        self.assertFalse(limiter.full)
+        self.assertEqual(w.writing, [])
+        self.assertEqual(w.written, [])
 
-        self.assertEqual((yield d), 1)
-        self.assertFalse(limiter.full)
+        yield limiter.add(w.write, 1)
+        self.assertEqual(w.writing, [1])
+        self.assertEqual(w.written, [])
 
-        d3 = limiter.add(Deferred())
-        d = limiter.when_not_full()
+        yield limiter.add(w.write, 2)
+        self.assertEqual(w.writing, [1, 2])
+        self.assertEqual(w.written, [])
 
-        self.assertTrue(limiter.full)
-        d3.callback(None)
-        self.assertFalse(limiter.full)
+        d3 = limiter.add(w.write, 3)
+        self.assertEqual(w.writing, [1, 2])
+        self.assertEqual(w.written, [])
 
-        self.assertEqual((yield d), 1)
-        self.assertFalse(limiter.full)
+        d4 = limiter.add(w.write, 4)
+        self.assertEqual(w.writing, [1, 2])
+        self.assertEqual(w.written, [])
 
-    @inlineCallbacks
-    def test_when_not_full_errback(self):
-        limiter = DeferredLimiter(2)
-        d1 = limiter.add(Deferred())
-        limiter.add(Deferred())
-        d = limiter.when_not_full()
-
-        self.assertTrue(limiter.full)
-        d1.errback(Exception())
-        self.assertFalse(limiter.full)
-
-        self.assertEqual((yield d), 1)
-        self.assertFalse(limiter.full)
-
-        d3 = limiter.add(Deferred())
-        d = limiter.when_not_full()
-
-        self.assertTrue(limiter.full)
-        d3.errback(Exception())
-        self.assertFalse(limiter.full)
-
-        self.assertEqual((yield d), 1)
-
-    @inlineCallbacks
-    def test_when_not_full_immediate(self):
-        limiter = DeferredLimiter(2)
-        d1 = limiter.add(Deferred())
-        self.assertEqual((yield limiter.when_not_full()), 1)
-
-    @inlineCallbacks
-    def test_cancel(self):
-        cancelled = []
-
-        def canceller(d):
-            d.callback(None)
-            cancelled.append(d)
-
-        limiter = DeferredLimiter(3)
-        d1 = limiter.add(Deferred(canceller))
-        d2 = limiter.add(Deferred(canceller))
-        d3 = limiter.add(Deferred(canceller))
-        d3.callback(None)
-
-        self.assertEqual(cancelled, [])
-        limiter.cancel()
-
-        yield d1
-        yield d2
-
-        self.assertEqual(cancelled, [d1, d2])
-
-    @inlineCallbacks
-    def test_errback(self):
-        errs = []
-
-        def errback(f):
-            errs.append(f.value)
-
-        limiter = DeferredLimiter(2, errback)
-        d1 = limiter.add(Deferred())
-        limiter.add(Deferred())
-
-        e1 = Exception()
-        d1.errback(e1)
-
-        yield d1
-        self.assertEqual(errs, [e1])
-
-        d3 = limiter.add(Deferred())
-        e2 = Exception()
-        d3.errback(e2)
-
+        yield w.next()
         yield d3
+        self.assertEqual(w.writing, [2, 3])
+        self.assertEqual(w.written, [1])
+
+        yield w.next()
+        yield d4
+        self.assertEqual(w.writing, [3, 4])
+        self.assertEqual(w.written, [1, 2])
+
+        yield w.next()
+        self.assertEqual(w.writing, [4])
+        self.assertEqual(w.written, [1, 2, 3])
+
+        yield w.next()
+        self.assertEqual(w.writing, [])
+        self.assertEqual(w.written, [1, 2, 3, 4])
+
+    @inlineCallbacks
+    def test_add_err(self):
+        errs = []
+        w = ManualWritable()
+        limiter = TaskLimiter(2, lambda f: errs.append(f.value))
+
+        e1, e2, e4 = [Exception() for i in range(3)]
+        yield limiter.add(w.write, 1)
+        yield limiter.add(w.write, 2)
+        d3 = limiter.add(w.write, 3)
+        d4 = limiter.add(w.write, 4)
+
+        self.assertEqual(errs, [])
+        self.assertEqual(w.writing, [1, 2])
+        self.assertEqual(w.written, [])
+
+        yield w.err(e1)
+        yield d3
+        self.assertEqual(errs, [e1])
+        self.assertEqual(w.writing, [2, 3])
+        self.assertEqual(w.written, [])
+
+        yield w.err(e2)
+        yield d4
         self.assertEqual(errs, [e1, e2])
+        self.assertEqual(w.writing, [3, 4])
+        self.assertEqual(w.written, [])
+
+        yield w.next()
+        self.assertEqual(w.writing, [4])
+        self.assertEqual(w.written, [3])
+
+        yield w.err(e4)
+        self.assertEqual(errs, [e1, e2, e4])
+        self.assertEqual(w.writing, [])
+        self.assertEqual(w.written, [3])
